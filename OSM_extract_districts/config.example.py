005604c235aa0c3be2ada48f851ebff7ed817337
trolleway <trolleway@yandex.ru>
2016-10-11 15:46:33 +0300

    Выгрузка районов

---
+++ OSM_extract_districts/config.example.py
@@ -0,0 +1,8 @@
+
+
+
+
+psycopg2_postgresql_connection_string="host= user= dbname= password= "
+osm2pgsql='--host localhost --database gis --username user'
+ogr2ogr_pg=psycopg2_postgresql_connection_string
+pgpass=''
