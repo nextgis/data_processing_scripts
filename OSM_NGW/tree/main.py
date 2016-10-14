99ae335616cbe3e3e59925bb5970ade450c5b0ed
trolleway <trolleway@yandex.ru>
2016-10-14 16:51:33 +0300

    develop new osm_ngw script with auto-update

---
+++ OSM_NGW/tree/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Project: Выгрузка слоя из OSM в NGW
+# Author: Artem Svetlov <artem.svetlov@nextgis.com>
+# Copyright: 2016, NextGIS <info@nextgis.com>
+
+
+import process
+
+# Все функции обработки записаны в файле process.py
+# Для работы: запускается python main.py а в этом файле расскоментируются строки запуска отдельных функций
+
+
+#Этот скрипт запускается из верхней папки
+
+processor=process.Processor()
+processor.osmimport('data')
+processor.postgis2geojson('planet_osm_point')
+#os.system('python update_ngw_from_geojson.py  --ngw_url '+config.ngw_url+' --ngw_resource_id 649 --ngw_login '+config.ngw_login+' --ngw_password '+config.ngw_password+' --check_field road_id --filename routes_with_refs.geojson')
+
+
+
+
