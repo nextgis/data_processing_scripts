d626e34993cb0ac8a5fa8aaf64269207bf2352a3
trolleway <trolleway@yandex.ru>
2016-10-17 17:55:59 +0300

    tune

--- OSM_extract_POI_in_grid/process.py
+++ OSM_extract_POI_in_grid/process.py
@@ -351,8 +351,8 @@ SELECT special_point.osm_id FROM special_point LEFT JOIN boundary  ON ST_Interse
                 y=y+ystep
                 ynum=y // ystep
 
-                print 'y=',str(y)
-                print 'ynum=',str(ynum)
+                #print 'y=',str(y)
+                #print 'ynum=',str(ynum)
 
                 str1='{ "type": "Feature", "properties": { "id": null, "x": '+str(int(xnum))+', "y": '+str(int(ynum))+' }, "geometry": { "type": "Polygon", "coordinates":'
 
@@ -370,10 +370,10 @@ SELECT special_point.osm_id FROM special_point LEFT JOIN boundary  ON ST_Interse
             print 'Сетка из grid.geojson импортируется в PostGIS одной операцией. В таблице всё время держится по одному столбцу сетки.'
 
             sql="TRUNCATE grid4326; INSERT INTO grid4326 SELECT (ST_GeomFromText('POLYGON  ',4326)), {xnum} AS x, generate_series({ynummin},{ynummax},{ystep}) AS y  ".format(xnum=str(xnum),ynummin=ynummin, ynummax=(totaly+ynummin), ystep=1 )
-            print sql
+            #print sql
             
             cmd='ogr2ogr  -f "PostgreSQL" PG:"{ogr2ogr_pg}" "grid.geojson" -nln grid4326 -overwrite -t_srs EPSG:4326'.format(ogr2ogr_pg=config.ogr2ogr_pg)
-            print cmd
+            #print cmd
             quit()
             os.system(cmd)
         
