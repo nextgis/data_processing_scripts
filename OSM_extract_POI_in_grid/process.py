99cb0b396b181cd524b52f65f52de465d37e4190
Artem Svetlov <trolleway@yandex.ru>
2016-10-19 02:12:28 +0300

    Update process.py

--- OSM_extract_POI_in_grid/process.py
+++ OSM_extract_POI_in_grid/process.py
@@ -346,14 +346,14 @@ ORDER BY userid, timestamp desc;
 		
 	    #Проверка: есть ли POI в этом столбце	
             gridColumnWKT='{x1} {y1},{x2} {y1},{x2} {y2},{x1} {y2}, {x1} {y1}'.format(x1=x, x2=x+xstep,y1=ymax, y2=ymin)
-            sql='''SELECT COUNT(special_point.*) AS cnt FROM special_point WHERE ST_Intersects(wkb_geometry,  ST_GeomFromText(\''''+bbox+'''\',4326))'''
+            sql='''SELECT COUNT(special_point.*) AS cnt FROM special_point WHERE ST_Intersects(wkb_geometry,  ST_GeomFromText(\''''+gridColumnWKT+'''\',4326))'''
             print sql
             self.cursor.execute(sql)
             self.conn.commit()
             rows = self.cursor.fetchall()
             bbox=''
             for row in rows:
-                cnt=row2[0]
+                cnt=row[0]
             if str(cnt)=='0':
                 print 'Этот столбец сетки не попадает в страну, пропуск'
                 continue
