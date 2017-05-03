6d0ffa351fc31ce5c8f4e430d7aa8360d0958e31
Artem Svetlov <trolleway@yandex.ru>
2017-05-03 15:07:46 +0300

    Update unpack_landsat7.sh

--- landsat_unpack/unpack_landsat7.sh
+++ landsat_unpack/unpack_landsat7.sh
@@ -15,7 +15,7 @@ ls -1 *tar.gz | sed -e 's/\..*$//' > scenes.list
 while read s; do  gdalbuildvrt -separate $s.vrt ${s}_B4.TIF ${s}_B3.TIF ${s}_B2.TIF; done < scenes.list
 
 #Конвертирование полигона обрезки в систему координат растра
-ogr2ogr -progress -f "GeoJSON" boundary.geojson boundary_reprojected.geojson  -t_srs EPSG:32650
+ogr2ogr -progress -f "GeoJSON"  boundary_reprojected.geojson boundary.geojson -t_srs EPSG:32650
 
 #Обрезка по полигональному geojson. Он должен быть в той же CRS, что и космоснимки, у меня - EPSG:32650
 while read s; do  gdalwarp -dstnodata 0 -co COMPRESS=JPEG -co JPEG_QUALITY=95  -cutline boudary_reprojected.geojson  $s.vrt ../$s.tif ; done < scenes.list
