accc6e06c216296482b89a72ad3d4acbff5c3494
Artem Svetlov <trolleway@yandex.ru>
2018-03-23 12:55:31 +0300

    Create run.sh

---
+++ adm_cmp/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x #echo on
+
+
+admfile='Муниципальные_образования_уровень_6_на 01.01.2018.xlsx'
+
+echo "Reading config...."
+source config.cfg
+
+#ogr2ogr -progress -overwrite -nln adm -f "PostgreSQL"   PG:"host=$host dbname=$dbname" $admfile 
+
