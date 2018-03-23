0c2e28f25bdbcfa2f335ab788853b2e3a3e420a5
Artem Svetlov <trolleway@yandex.ru>
2018-03-23 17:39:17 +0300

    Update run.sh

--- adm_cmp/run.sh
+++ adm_cmp/run.sh
@@ -8,5 +8,7 @@ admfile='Муниципальные_образования_уровень_6_на
 echo "Reading config...."
 source config.cfg
 
-ogr2ogr -progress -overwrite -nln adm -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" 
+ogr2ogr -progress -overwrite -nln adml1 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Количество МО в субъектах"
+ogr2ogr -progress -overwrite -nln adml2 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Таблица Наимен. и ОКТМО"
+ogr2ogr -progress -overwrite -nln adml3 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Имена полей"
 
