f985d3d99cccc55b1c4c282bca7e32eef7616d1f
Artem Svetlov <trolleway@yandex.ru>
2018-03-23 17:52:52 +0300

    Update run.sh

--- adm_cmp/run.sh
+++ adm_cmp/run.sh
@@ -8,7 +8,18 @@ admfile='Муниципальные_образования_уровень_6_на
 echo "Reading config...."
 source config.cfg
 
-ogr2ogr -progress -overwrite -nln adml1 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Количество МО в субъектах"
-ogr2ogr -progress -overwrite -nln adml2 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Таблица Наимен. и ОКТМО"
-ogr2ogr -progress -overwrite -nln adml3 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Имена полей"
+ogr2ogr -progress -overwrite -nln adm1 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Количество МО в субъектах"
+ogr2ogr -progress -overwrite -nln adm2 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Таблица Наимен. и ОКТМО"
+ogr2ogr -progress -overwrite -nln adm3 -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Имена полей"
 
+
+admfile='adm4.gpkg'
+
+ogr2ogr -progress -overwrite -nln admosm -f "PostgreSQL"   PG:"host=$host dbname=$dbname" "$admfile" "Имена полей"
+
+
+SQL=$(cat <<-ENDTEXT
+
+-- опционально
+ENDTEXT
+)
