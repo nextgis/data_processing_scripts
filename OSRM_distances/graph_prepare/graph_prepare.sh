4b886460ce6d67bf118b948ac463577ff06b0f2a
trolleway <trolleway@yandex.ru>
2018-04-24 14:53:37 +0300

    more automated preparing graph

--- OSRM_distances/graph_prepare/graph_prepare.sh
+++ OSRM_distances/graph_prepare/graph_prepare.sh
@@ -1,12 +1,14 @@
 
 
 filename='RU-MOS'
+dbname='gis'
+
 wget --timestamping http://data.gis-lab.info/osm_dump/dump/latest/${filename}.osm.pbf
 osmconvert $filename.osm.pbf -B=area.poly -o=background_clipped.o5m
 osmfilter background_clipped.o5m --drop-author --keep="building=" --out-o5m >background-filtered.o5m
 osmconvert background-filtered.o5m -o=background.pbf
 
-osm2pgsql --database processing_osm_ch1 --latlon background.pbf
+osm2pgsql --database $dbname --latlon background.pbf
 rm background-filtered.o5m
 
 
@@ -18,13 +20,13 @@ rm background-filtered.o5m
 #Подготовка файла графа
 filename='RU-MOS'
 wget --timestamping http://data.gis-lab.info/osm_dump/dump/latest/${filename}.osm.pbf
-osmconvert $filename.osm.pbf -B=area.poly -o=RU-MOS_clipped.osm.pbf
-mv ".osm.pbf" "RU-MOS_clipped.osm.pbf"
+osmconvert $filename.osm.pbf -B=area.poly -o=buildings_clipped.osm.pbf
+mv ".osm.pbf" "buildings_clipped.osm.pbf"
 
-#далее из каталога osrm-backend
+#далее выполнять ищиз каталога osrm-backend
 
 
-filenamecl='RU-MOS_clipped'
+filenamecl='background_clipped'
 osrm-extract /home/trolleway/GIS/GIS/project98_isodistances/my6/data_processing_scripts/OSRM_distances/graph_prepare/$filenamecl.osm.pbf
 osrm-contract /home/trolleway/GIS/GIS/project98_isodistances/my6/data_processing_scripts/OSRM_distances/graph_prepare/$filenamecl.osrm
 osrm-routed --threads=6 /home/trolleway/GIS/GIS/project98_isodistances/my6/data_processing_scripts/OSRM_distances/graph_prepare/$filenamecl.osrm
