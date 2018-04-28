171ffb966a670c13de473b281fceae717e96049c
Artem Svetlov <trolleway@yandex.ru>
2018-04-28 16:33:02 +0300

    Create main.py

---
+++ OAM_download/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Project: выкачивание с openaerialmap
+# Author: Artem Svetlov <artem.svetlov@nextgis.com>
+# Copyright: 2018, NextGIS <info@nextgis.com>
+
+
+import os
+import requests
+
+
+
+
+
+class Processor:
+
+    statistic={}
+        #Define our connection string
+
+    connection = None
+    cursor = None      
+     
+        # get a connection, if a connect cannot be made an exception will be raised here
+
+
+    
+    geojson_header3857='''{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::3857" } },
+                                                                                
+"features": [
+
+    '''
+    geojson_footer='''
+]
+}
+    '''
+
+
+    def __init__(self, pg_conn=None):
+        self.pg_conn = pg_conn
+        self.conn = psycopg2.connect(pg_conn)
+        self.conn.autocommit = True #для vaccuum
+     
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        self.cursor = self.conn.cursor()
+
+
+
+import argparse
+
+
+def argparser_prepare():
+
+    class PrettyFormatter(argparse.ArgumentDefaultsHelpFormatter,
+        argparse.RawDescriptionHelpFormatter):
+
+        max_help_position = 35
+
+
+    parser = argparse.ArgumentParser(description='''Ge
+    ''',
+            formatter_class=PrettyFormatter)
+
+
+    
+
+    parser.add_argument('-pg', '--pg_conn', type=str, default='',
+                        help='PostGIS connection string to osm dump')
+    parser.add_argument('-s', '--starts', type=str, default='starts.geojson',
+                        help='Point geodata file')
+    parser.add_argument('-d', '--distance', type=str, default='1000',
+                        help='Distance in meters')
+    parser.add_argument('-c', '--calc_distance', type=str, default='1000',
+                        help='Distance for initial selection of calc points. Ideally should be same as distance, but for big city may be 50%% of distance to spped up')
+    parser.add_argument('--overlap',
+                    default='independed',
+                    choices=[ 'overlapped', 'independed'],
+                    help='overlapped: return overlapped polygons; independed: use only distance from nearest start, return touched polygons')
+
+
+
+    parser.epilog = \
+        '''Samples: 
+%(prog)s --pg_conn "dbname=osm_ch3" --filename starts.shp
+''' \
+        % {'prog': parser.prog}
+    return parser
+
+
+parser = argparser_prepare()
+args = parser.parse_args()
+processor=Processor(pg_conn=args.pg_conn)
+#processor.generate_filter_string() #Generate string with tags for osmfilter
+#processor.osmimport('moscow_russia')
+processor.pointsimport(args.starts)
+processor.isodistances(distance=args.distance,cutdistance=args.calc_distance,overlap=args.overlap)
+#processor.isodistances2geojson(isodistances)
+
+
