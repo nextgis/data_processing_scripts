43a5bc2049c1d00af4ee882534b70fb79e93520e
trolleway <trolleway@yandex.ru>
2018-04-28 17:50:49 +0300

    midofied

--- OAM_download/main.py
+++ OAM_download/main.py
@@ -22,10 +22,15 @@ class Processor:
         endpoint = 'https://api.openaerialmap.org/meta'
         query = endpoint + '?bbox=37.3193,55.4899,37.9457,56.0097&gsd_to=1&gsd_from=0.001&acquisition_from=2014-01-01&acquisition_to=2018-01-01&limit=100'
         print query
-        os.system('wget -O response.json '+query)
-        listresp = json.loads((open('response.json')))
-        for scene in listresp:
+        os.system('wget -O response.json "'+query + '"')
+        listresp = json.load((open('response.json')))
+        urllist = open('urllist.txt', 'w')
+        for scene in listresp['results']:
+            urllist.write(scene['uuid']+"\n")
             print scene['uuid']
+        urllist.close()
+
+        #os.system('cat urllist.txt | parallel --gnu "wget --timestamping {}"')
 
 
 import argparse
