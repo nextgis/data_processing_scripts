9421b98348b29824edeb97e6450c381d701b4112
trolleway <trolleway@yandex.ru>
2020-01-24 19:14:44 +0300

    upd

--- OSM_merge_graphs2/process.sql
+++ OSM_merge_graphs2/process.sql
@@ -8,6 +8,73 @@ SELECT (dumppoints).path[1], (dumppoints).geom FROM
 
 --Найти пересечения новых линий между собой.
 --Добавить пересечения новых линий в линии.
+
+DROP TABLE IF EXISTS intersections;
+DROP TABLE IF EXISTS joining_union;
+DROP TABLE IF EXISTS joining_segments;
+
+CREATE TABLE intersections AS
+SELECT DISTINCT (ST_DUMP(ST_INTERSECTION(a.wkb_geometry, b.wkb_geometry))).geom AS ix
+FROM user_highways a
+INNER JOIN user_highways b
+ON ST_INTERSECTS(a.wkb_geometry,b.wkb_geometry)
+WHERE geometrytype(st_intersection(a.wkb_geometry,b.wkb_geometry)) = 'POINT';
+
+CREATE INDEX ON intersections USING gist(ix);
+
+CREATE TABLE joining_union AS
+SELECT ST_UNION(wkb_geometry) as geom
+FROM user_highways;
+
+CREATE INDEX ON joining_union USING gist(geom);
+
+CREATE TABLE joining_segments AS
+SELECT (ST_DUMP(ST_SPLIT(a.geom,b.ix))).geom AS wkb_geometry,
+ROW_NUMBER () OVER () as id
+FROM joining_union a
+INNER JOIN intersections b
+ON ST_INTERSECTS(a.geom, b.ix)
+GROUP BY wkb_geometry;
+
+
+
+-----------------------
+/*
+CREATE INDEX ON intersections USING gist(ix);
+CREATE TABLE joining_union AS
+SELECT ST_UNION(wkb_geometry) as wkb_geometry
+FROM joining;
+CREATE INDEX ON joining_union USING gist(wkb_geometry);
+CREATE TABLE joining_segments AS
+SELECT (ST_DUMP(ST_SPLIT(a.wkb_geometry,b.ix))).geom AS wkb_geometry
+FROM joining_union a
+INNER JOIN intersections b
+ON ST_INTERSECTS(a.wkb_geometry, b.ix);
+ALTER TABLE joining_segments ADD COLUMN id SERIAL PRIMARY KEY;
+DROP TABLE intersections;
+DROP TABLE joining_union;
+*/
+
+
+
+DROP TABLE IF EXISTS split;
+CREATE TABLE split AS(
+SELECT
+    (ST_Dump(ST_Split(ST_Snap(a.wkb_geometry, b.ix, 0.00001),b.ix))).geom AS wkb_geometry,
+    a.highway
+FROM
+    user_highways a
+JOIN
+    intersections b
+ON
+    ST_DWithin(b.ix, a.wkb_geometry, 0.00001)
+);
+
+ALTER TABLE split ADD COLUMN id SERIAL PRIMARY KEY;
+CREATE INDEX ON split USING gist(wkb_geometry);
+
+-- =============================================================================
+
 --Найти пересечения новых линий со старыми
 --Добавить точки в новые линии.
 --Добавить точки в старые линии.
