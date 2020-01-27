748e795de06dfc99019bf623fd4b51b779cab40d
trolleway <trolleway@yandex.ru>
2020-01-27 12:55:52 +0300

    upd

--- OSM_merge_graphs2/process.sql
+++ OSM_merge_graphs2/process.sql
@@ -36,42 +36,9 @@ INNER JOIN intersections b
 ON ST_INTERSECTS(a.geom, b.ix)
 GROUP BY wkb_geometry;
 
+-- получился слой joining_segments с линиями, разрезаными по перекрёсткам, но без атрибутов
+-- надо заменить на joining_segments
 
-
------------------------
-/*
-CREATE INDEX ON intersections USING gist(ix);
-CREATE TABLE joining_union AS
-SELECT ST_UNION(wkb_geometry) as wkb_geometry
-FROM joining;
-CREATE INDEX ON joining_union USING gist(wkb_geometry);
-CREATE TABLE joining_segments AS
-SELECT (ST_DUMP(ST_SPLIT(a.wkb_geometry,b.ix))).geom AS wkb_geometry
-FROM joining_union a
-INNER JOIN intersections b
-ON ST_INTERSECTS(a.wkb_geometry, b.ix);
-ALTER TABLE joining_segments ADD COLUMN id SERIAL PRIMARY KEY;
-DROP TABLE intersections;
-DROP TABLE joining_union;
-*/
-
-
-
-DROP TABLE IF EXISTS split;
-CREATE TABLE split AS(
-SELECT
-    (ST_Dump(ST_Split(ST_Snap(a.wkb_geometry, b.ix, 0.00001),b.ix))).geom AS wkb_geometry,
-    a.highway
-FROM
-    user_highways a
-JOIN
-    intersections b
-ON
-    ST_DWithin(b.ix, a.wkb_geometry, 0.00001)
-);
-
-ALTER TABLE split ADD COLUMN id SERIAL PRIMARY KEY;
-CREATE INDEX ON split USING gist(wkb_geometry);
 
 -- =============================================================================
 
