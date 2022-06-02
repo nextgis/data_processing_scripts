77b03b9d95c560816d0bebb94f0a2dff636c69d4
Maxim Dubinin <maxim.dubinin@nextgis.com>
2022-06-02 11:49:47 +0300

    do not try to calc features

--- intersection_batch/run.py
+++ intersection_batch/run.py
@@ -74,7 +74,6 @@ for dirpath, dnames, fnames in os.walk(regionsdir):
             
 assert(len(regions_layers)>0)
 total=len(regions_layers)
-
 regions_layers.sort()
 
 exportdir = os.path.join(os.path.dirname(file1),'intersection')
@@ -87,14 +86,14 @@ layer1 = ds1.GetLayer()
 assert layer1 is not None
 assert layer1.GetFeatureCount() > 0
 
-cnt=0
+cnt = 0
 for boundary_filename in regions_layers:
-    cnt=cnt+1
+    cnt = cnt + 1
     cont = True
     print('{cnt}/{total} {name}'.format(cnt=cnt,total=total,name=os.path.splitext(os.path.basename(boundary_filename))[0]))
     
     dst_prefix = os.path.splitext(os.path.basename(boundary_filename))[0]
-    out_filename = os.path.join(exportdir,dst_prefix+'.gpkg')
+    out_filename = os.path.join(exportdir, dst_prefix + '.gpkg')
     if os.path.exists(out_filename):
         print('extract skipped, result file already exists')
         cont = False
@@ -110,17 +109,14 @@ for boundary_filename in regions_layers:
         
         v1=layer1.GetFeatureCount()
         layer1.SetSpatialFilter(feature.GetGeometryRef())
-        v2=layer1.GetFeatureCount()
-        if v2 == 0:
-            print('extract skipped, no features found')
-            continue
+        #v2=layer1.GetFeatureCount()
+        #if v2 == 0:
+        #    print('extract skipped, no features found')
+        #    continue
 
         opt = []
         drv = ogr.GetDriverByName('GPKG')
         out_ds = drv.CreateDataSource(out_filename, options=opt)
         
-        layer_out = out_ds.CopyLayer(layer1, 'intersection', 
-            options=[
-            ]
-        )
+        layer_out = out_ds.CopyLayer(layer1, 'intersection', options=[])
 
